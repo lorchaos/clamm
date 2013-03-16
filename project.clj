83fc3ecd58e3d5e9b9e455bb7354bf179c0e8aaa
rlorca <lorchaos@gmail.com>
2013-03-16 13:59:23 +0100

    Fixed project descriptor

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clamm "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple implementation of the Damm algorithm"
+  :url "https://github.com/lorchaos/clamm"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
